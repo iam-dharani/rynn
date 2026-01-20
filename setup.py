24e25792605baab16e6d537a6a95649e6188d49a
mehr <ubuntu@ip-172-31-25-176.ec2.internal>
2026-01-20 09:09:45 +0000

    first commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="python-jenkins-demo",
+    version="1.0.0",
+    py_modules=["app"],
+    install_requires=[],
+)
+
---
+++ test_app.py
@@ -0,0 +1,9 @@
+import app
+
+def test_add():
+    assert app.add(2, 3) == 5
+
+
+def test_subtract():
+    assert app.subtract(5, 3) == 2
+
